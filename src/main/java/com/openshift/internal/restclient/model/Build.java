bf520f9c3dd6285c1df42a971cfeccb15af29e51
S3873 <martin.kaderabek@s-itsolutions.at>
2016-05-09 22:21:10 +0200

    Add some Build information

--- src/main/java/com/openshift/internal/restclient/model/Build.java
+++ src/main/java/com/openshift/internal/restclient/model/Build.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors: Red Hat, Inc.
  ******************************************************************************/
 package com.openshift.internal.restclient.model;
@@ -19,14 +19,22 @@ import com.openshift.restclient.model.IBuild;
 /**
  * @author Jeff Cantrill
  */
-public class Build extends KubernetesResource implements IBuild{
-	
+public class Build extends KubernetesResource implements IBuild {
+
 	private static final String BUILD_MESSAGE = "status.message";
 	private static final String BUILD_PODNAME = "podName";
 	private static final String BUILD_STATUS = "status.phase";
+	private static final String GIT = "spec.revision.git";
+	private static final String GIT_COMMIT = GIT + ".commit";
+	private static final String GIT_COMMITTER_NAME = GIT + ".committer.name";
+	private static final String GIT_COMMITTER_EMAIL = GIT + ".committer.email";
+	private static final String GIT_MESSAGE = GIT + ".message";
 
+	private static final String SOURCE = "spec.source";
+	private static final String SOURCE_GIT_URI = SOURCE + ".git.uri";
+	private static final String SOURCE_GIT_REF = SOURCE + ".git.ref";
 
-	public Build(ModelNode node, IClient client, Map<String, String []> propertyKeys) {
+	public Build(ModelNode node, IClient client, Map<String, String[]> propertyKeys) {
 		super(node, client, propertyKeys);
 		CapabilityInitializer.initializeCapabilities(getModifiableCapabilities(), this, client);
 	}
@@ -46,5 +54,34 @@ public class Build extends KubernetesResource implements IBuild{
 		return asString(BUILD_PODNAME);
 	}
 
-	
+	@Override
+	public String getGitCommit() {
+		return asString(GIT_COMMIT);
+	}
+
+	@Override
+	public String getGitCommitterName() {
+		return asString(GIT_COMMITTER_NAME);
+	}
+
+	@Override
+	public String getGitCommitterEmail() {
+		return asString(GIT_COMMITTER_EMAIL);
+	}
+
+	@Override
+	public String getGitMessage() {
+		return asString(GIT_MESSAGE);
+	}
+
+	@Override
+	public String getSourceGitUri() {
+		return asString(SOURCE_GIT_URI);
+	}
+
+	@Override
+	public String getSourceGitRef() {
+		return asString(SOURCE_GIT_REF);
+	}
+
 }
--- src/main/java/com/openshift/restclient/model/IBuild.java
+++ src/main/java/com/openshift/restclient/model/IBuild.java
@@ -3,7 +3,7 @@
  * All rights reserved. This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors: Red Hat, Inc.
  ******************************************************************************/
 package com.openshift.restclient.model;
@@ -15,20 +15,36 @@ public interface IBuild extends IResource {
 
 	/**
 	 * Returns the status of the buld
+	 *
 	 * @return
 	 */
 	String getStatus();
-	
+
 	/**
 	 * Returns the details about the status of this build
+	 *
 	 * @return
 	 */
 	String getMessage();
-	
+
 	/**
 	 * Returns the name of the pod running the build
+	 *
 	 * @return
 	 */
 	@Deprecated
 	String getPodName();
+
+	public String getGitCommit();
+
+	public String getGitCommitterName();
+
+	public String getGitCommitterEmail();
+
+	public String getGitMessage();
+
+	public String getSourceGitUri();
+
+	public String getSourceGitRef();
+
 }
